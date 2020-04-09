a178cc02b98c104a61d5f88e3b92183d68f3dd99
Auralcat <ashleyzt@hi2.in>
2020-04-09 20:28:38 -0300

    Stub README.md, initial commit
    Get the ball rolling! o/

---
+++ README.md
@@ -0,0 +1,17 @@
+# Projectile-phoenix
+
+<!-- Cool image goes here -->
+
+This project aims to offer quicker navigation to Phoenix-related files at first:
+
+- Find controller
+- Find context
+- Jump to test
+- Run mix tasks
+
+The idea is to have a single navigation responsibility, offloading other
+features to other packages.
+
+## Why not use Alchemist?
+Alchemist offers navigation between Phoenix files with a minor mode, but it has
+been unmaintained for some time now.
---
+++ projectile-phoenix.el
@@ -0,0 +1,33 @@
+;;; projectile-phoenix.el --- Minor mode for Phoenix projects based on projectile-mode
+
+;; Copyright (C) 2013 Adam Sokolnicki
+
+;; Author:            Miriam Retka <miriamretka@tutanota.com>
+;; URL:               <insert_url_here>
+;; Package-Version: 20200408
+;; Version:           0.1
+;; Keywords:          phoenix, projectile
+;; Package-Requires:  ((emacs "24.3") (projectile "0.12.0"))
+
+;; This file is NOT part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary
+;; This project is a helper thingy for Phoenix projects, much like
+;; projectile-rails. But for Phoenix projects.
